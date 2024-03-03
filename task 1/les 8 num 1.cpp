9ed19f5604951d1b2fcb620c02772e05a6a67405
sarkrys <filatovaroslav2008@gmail.com>
2024-03-03 18:09:08 +0300

    fix num1

--- task 1/les 8 num 1.cpp
+++ task 1/les 8 num 1.cpp
@@ -35,19 +35,19 @@ public:
 
     bool operator<=(const Fraction& other) const
     {
-        return (*this < other || *this == other);
+        return (*this == other || *this < other);
     }
 
     bool operator>=(const Fraction& other) const
     {
-        return (*this > other || *this == other);
+        return (*this == other || *this > other);
     }
 };
 
 int main()
 {
     Fraction f1(4, 3);
-    Fraction f2(6, 11);
+    Fraction f2(8, 6);
 
     std::cout << "f1" << (f1 == f2 ? " == " : " not == ") << "f2" << '\n';
     std::cout << "f1" << (f1 != f2 ? " != " : " not != ") << "f2" << '\n';
